2cc4ffa3c2f71adfdfc82bf8f74256f14e131910
SamLams24 <attiogbesamuel310@gmail.com>
2025-05-09 03:20:22 +0000

    adding swagger annotations

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
+    console.log("MONGO_URI =", process.env.MONGO_URI);
     await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB connecté");
   } catch (error) {
